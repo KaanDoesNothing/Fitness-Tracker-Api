65d2d27121077806fa29f0866d0346de3a1e7ca9
KaanDoesNothing <kaanyt123@gmail.com>
2022-04-03 17:26:09 +0200

    Trying to backport v2 to v1 for the old users

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,7 @@ import * as db from "./db";
 
 db.setup();
 
+import {router as v1} from "./routes/v2";
 import {router as v2} from "./routes/v2";
 
 const app = new koa();
@@ -13,6 +14,7 @@ const app = new koa();
 app.use(koaCors());
 app.use(koaBody());
 
+app.use(v1.middleware());
 app.use(v2.middleware());
 
 app.use((ctx: any, next: any) => {
