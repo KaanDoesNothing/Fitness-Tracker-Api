56a1c44cbd34c4c7d543f6167f22f30e82f76990
KaanDoesNothing <kaanyt123@gmail.com>
2022-04-03 18:57:59 +0200

    Improved security, Slightly increases latency though so might find another solution

--- src/middlware.ts
+++ src/middlware.ts
@@ -1,12 +1,19 @@
 import {jwtDecode} from "./utils";
 import {Context, Next} from "koa";
+import { User } from "./entities/user";
 
 export const handleSession = async (ctx: Context, next: Next) => {
     let headers = ctx.request.headers;
     if(headers["authorization"]) {
         let token = headers["authorization"].split(" ")[1];
 
-        ctx.session = jwtDecode(token);
+        let decoded: any = jwtDecode(token);
+
+        let user = await User.findOne<User>({where: {email: decoded.email}});
+
+        if(!user) return ctx.body = {error: "Invalid account."};
+
+        ctx.session = decoded;
     }
     await next();
 }
