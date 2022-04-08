c6db11f34f5a6e1af2bc856b77a4b34ce9f26db0
KaanDoesNothing <kaanyt123@gmail.com>
2022-04-08 21:34:13 +0200

    Adding a basic chat

--- src/utils.js
+++ src/utils.js
@@ -3,15 +3,15 @@ const bcrypt = require("bcrypt");
 
 const secret = "075";
 
-let jwtSign = (data) => {
+const jwtSign = (data) => {
     return jwt.sign(data, secret);
 }
 
-let jwtDecode = (token) => {
+const jwtDecode = (token) => {
     return jwt.verify(token, secret);
 }
 
-let hashPassword = async (password, saltRounds = 10) => {
+const hashPassword = async (password, saltRounds = 10) => {
     try {
         // Generate a salt
         const salt = await bcrypt.genSalt(saltRounds);
@@ -26,7 +26,7 @@ let hashPassword = async (password, saltRounds = 10) => {
     return null;
 };
 
-let comparePassword = async (password, hash) => {
+const comparePassword = async (password, hash) => {
     try {
         // Compare password
         return await bcrypt.compare(password, hash);
