e37f74fd023d0814b71445464dfd274712ea50be
KaanDoesNothing <kaanyt123@gmail.com>
2022-04-09 14:07:37 +0200

    Adding a basic chat

--- src/api.js
+++ src/api.js
@@ -3,14 +3,17 @@ const koaBody = require("koa-body");
 const koaCors = require("@koa/cors");
 
 const router = require("./routes/v1");
+const v2 = require("./routes/v2");
 
 const app = new koa();
 
 app.use(koaCors());
 app.use(koaBody());
 
+// app.use(router.middleware());
 app.use(router.routes());
 app.use(router.allowedMethods());
+app.use(v2.middleware());
 
 app.use((ctx, next) => {
     ctx.body = {error: "Not Found"};
